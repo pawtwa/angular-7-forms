7c460d215a2054c9d1fc95a3ae8230bc0c1e3826
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-02-21 09:30:46 +0100

    TD: accesing the form with @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,14 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // @ViewChild('f') form: ElementRef;
+  @ViewChild('f') form: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   submitForm(form: NgForm) {
-    
+    console.log(this.form);
   }
 }
